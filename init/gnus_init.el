0a667b887d0c712d636b5a90c647053da3061d3a
Vedat Hallac <vedathallac@gmail.com>
2011-03-01 02:36:59 +0200

    Add multiple SMTP (from same server) setup for gnus
    - Basically get the setu information from http://www.emacswiki.org/emacs/MultipleSMTPAccounts
      and modify it to get credentials from authinfo.gpg
    - Add SMTP credentials for my gmail accounts

--- init/gnus_init.el
+++ init/gnus_init.el
@@ -36,24 +36,18 @@
                 (gnus-check-new-newsgroups nil)
                 (gnus-ignored-newsgroups "^to\\.\\|^[0-9. 	]+\\( \\|$\\)\\|^[\”]\”[#’()]"))))
 
-(setq message-send-mail-function 'smtpmail-send-it
-      smtpmail-starttls-credentials '(("smtp.googlemail.com" 587 nil nil))
-      smtpmail-default-smtp-server "smtp.googlemail.com"
-      smtpmail-smtp-server "smtp.googlemail.com"
-      smtpmail-smtp-service 587)
-
 (setq tls-program `(,(concat
                       (if (boundp 'openssl-prg)
                           openssl-prg
                         "openssl")
                       " s_client -connect %h:%p -no_ssl2 -ign_eof")))
 
+;; html mail and foreign characters
+(setq mm-text-html-renderer 'w3m)
+(setq mm-coding-system-priorities '(utf-8))
+
 ;; Use openssl for TLS - gnutls behaves badly under Win32
 (require 'smtp-openssl)
-
-;; html mail stuff
-(setq mm-text-html-renderer 'w3m)
-
 
 (setq gnus-use-adaptive-scoring t)
 (setq gnus-score-expiry-days 60)
@@ -69,5 +63,74 @@
 (add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
 (setq gnus-topic-line-format "%i[ %0{%(%n (new: %A)%)%} ]\n")
 
-;; TODO: Sending mail from gmail-2 doesn't work due to SMTP setup. The From:
-;; address get overwritten by the mail address of the gmail-1 account.
+;; Set up multiple SMTP accounts
+;; Available SMTP accounts.
+;; Format for each entry is:
+;; (mech address alias host port user password key cert)
+;; - mech is one of 'plain, 'login, 'cram-md5, or 'ssl
+;; - alias is the server's alias. This is used to support multiple accounts on
+;;   same server. The authinfo entry should have a machine that matches the alias.
+;; - address is the e-mail address that uses this server
+;; - host is the SMTP host
+;; - port is the port to connect to
+;; - user is the user name for authentication. This parameter is optional.
+;; - password is the password for authentication. This parameter is optional.
+;; - key is ???
+;; - cert is ??? (certificate of the server?)
+(defvar smtp-accounts
+  '( (ssl "vedathallac@gmail.com" "gmail-1" "smtp.googlemail.com" 587)
+     (ssl "dys.wowace@gmail.com" "gmail-2" "smtp.googlemail.com" 587)))
+
+(defun set-smtp-common (alias server port &optional user password)
+  "Set the common fields of the supplied parameters"
+  (unless user
+    (let ((port-name (format "%s" port)))
+      (multiple-value-setq
+          (user password)
+        (auth-source-user-or-password '("login" "password") alias port-name))))
+  (setq smtpmail-auth-credentials (list (list server port user password))
+        smtpmail-smtp-server server
+        smtpmail-smtp-service port)
+  (message "Setting SMTP server to `%s:%s' for user `%s'." server port user))
+
+(defun set-smtp (mech alias server port &optional user password)
+  "Set related SMTP variables for supplied parameters."
+  (set-smtp-common alias server port user password)
+  (setq smtpmail-auth-supported (list mech)
+        smtpmail-starttls-credentials nil))
+
+(defun set-smtp-ssl (alias server port &optional user password key cert)
+  "Set related SMTP and SSL variables for supplied parameters."
+  (set-smtp-common alias server port user password)
+  (setq starttls-use-gnutls nil        ;use starttls-program
+        starttls-extra-arguments nil
+        smtpmail-starttls-credentials (list (list server port key cert))))
+
+(defun change-smtp ()
+  "Change the SMTP server according to the current from line."
+  (save-excursion
+    (loop with from = (save-restriction
+                        (message-narrow-to-headers)
+                        (message-fetch-field "from"))
+          for (auth-mech address . auth-spec) in smtp-accounts
+          when (string-match address from)
+          do (cond
+              ((memq auth-mech '(cram-md5 plain login))
+               (return (apply 'set-smtp (cons auth-mech auth-spec))))
+              ((eql auth-mech 'ssl)
+               (return (apply 'set-smtp-ssl auth-spec)))
+              (t (error "Unrecognized SMTP auth. mechanism: `%s'." auth-mech)))
+          finally (error "Cannot infer SMTP information."))))
+
+(require 'smtpmail)
+
+(defvar %smtpmail-via-smtp (symbol-function 'smtpmail-via-smtp))
+
+(defun smtpmail-via-smtp (recipient smtpmail-text-buffer)
+  ;; Not sure if this is the right way, but it seems to prevent the password
+  ;; lingering around in the variable.
+  (let ((smtpmail-auth-credentials nil))
+    (with-current-buffer smtpmail-text-buffer
+      (change-smtp))
+    (funcall (symbol-value '%smtpmail-via-smtp) recipient smtpmail-text-buffer)))
+
