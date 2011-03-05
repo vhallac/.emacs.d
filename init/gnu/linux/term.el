a5e33718343ef3a05218c9351b2d22fa9bee4347
Vedat Hallac <vedathallac@gmail.com>
2011-03-05 12:31:44 +0000

    Add some more terminal keys

--- init/gnu/linux/term.el
+++ init/gnu/linux/term.el
@@ -5,3 +5,12 @@
    ;; Use the xterm color initialization code.
    (xterm-register-default-colors)
    (tty-set-up-initial-frame-faces))
+
+;; Define sone terminal key codes.
+;; TODO: How compatible are these between different terminals?
+(define-key function-key-map "\e[1;3A" '[M-up])
+(define-key function-key-map "\e[1;3B" '[M-down])
+(define-key function-key-map "\e[1;3C" '[M-right])
+(define-key function-key-map "\e[1;3D" '[M-left])
+(define-key function-key-map "\e[13~" '[f3])
+(define-key function-key-map "\e[14~" '[f4])
