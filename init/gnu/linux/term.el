38aa596cdf23b81d547ce308585d3857bec90b76
Vedat Hallac <vedathallac@gmail.com>
2011-07-20 11:16:06 +0300

    Added some more key translations

--- init/gnu/linux/term.el
+++ init/gnu/linux/term.el
@@ -9,8 +9,12 @@
 ;; Define sone terminal key codes.
 ;; TODO: How compatible are these between different terminals?
 (define-key function-key-map "\e[1;3A" '[M-up])
+(define-key function-key-map "\e[1;5A" '[C-up])
 (define-key function-key-map "\e[1;3B" '[M-down])
+(define-key function-key-map "\e[1;5B" '[C-down])
 (define-key function-key-map "\e[1;3C" '[M-right])
+(define-key function-key-map "\e[1;5C" '[C-right])
 (define-key function-key-map "\e[1;3D" '[M-left])
+(define-key function-key-map "\e[1;5D" '[M-left])
 (define-key function-key-map "\e[13~" '[f3])
 (define-key function-key-map "\e[14~" '[f4])
