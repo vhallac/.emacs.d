a69c0b1505c2c37d862dd719efbd9f8539a71449
Vedat Hallac <vedathallac@gmail.com>
2011-07-22 10:13:13 +0300

    Added functions that deal with blocks and their keybindings
    The first function moves to start of a do..end or { .. } block. The second
    function converts a do..end block to {..} and vice versa.

--- config/customize-ruby-mode.el
+++ config/customize-ruby-mode.el
@@ -44,3 +44,55 @@
 (define-key ruby-mode-map (kbd "C-c d") 'yari-anything)
 (define-key ruby-mode-map (kbd "<tab>") 'yas/expand)
 (define-key ruby-mode-map (kbd "#") 'ruby-electric-strparam)
+(define-key ruby-mode-map (kbd "C-M-u") 'ruby-goto-containing-block-start)
+(define-key ruby-mode-map (kbd "C-c b") 'ruby-flip-containing-block-type)
+
+(defun ruby-get-containing-block ()
+  (let ((pos (point))
+        (block nil))
+    (save-match-data
+      (save-excursion
+        (catch 'break
+          ;; If in the middle of or at end of do, go back until at start
+          (while (and (not (looking-at "do"))
+                      (string-equal (word-at-point) "do"))
+            (backward-char 1))
+          ;; Keep searching for the containing block (i.e. the block that begins
+          ;; before our point, and ends after it)
+          (while (not block)
+            (if (looking-at "do\\|{")
+                (let ((start (point)))
+                  (ruby-forward-sexp)
+                  (if (> (point) pos)
+                      (setq block (cons start (point)))
+                    (goto-char start))))
+            (if (not (search-backward-regexp "do\\|{" (point-min) t))
+                (throw 'break nil))))))
+        block))
+
+(defun ruby-goto-containing-block-start ()
+  (interactive)
+  (let ((block (ruby-get-containing-block)))
+    (if block
+        (goto-char (car block)))))
+
+(defun ruby-flip-containing-block-type ()
+  (interactive)
+  (save-excursion
+    (let ((block (ruby-get-containing-block)))
+      (goto-char (car block))
+      (save-match-data
+        (let ((strings (if (looking-at "do")
+                           (cons
+                            (if (= 3 (count-lines (car block) (cdr block)))
+                                "do *\\(|[^|]+|\\)? *\n *\\(.*?\\) *\n *end"
+                              "do *\\(|[^|]+|\\)? *\\(\\(.*\n?\\)+\\) *end")
+                            "{ \\1 \\2 }")
+                         (cons
+                          "{ *\\(|[^|]+|\\)? *\\(\\(.*\n?\\)+\\) *}"
+                          (if (= 1 (count-lines (car block) (cdr block)))
+                              "do \\1\n\\2\nend"
+                            "do \\1\\2end")))))
+          (when (re-search-forward (car strings) (cdr block) t)
+            (replace-match (cdr strings) t)
+            (indent-region (match-beginning 0) (match-end 0))))))))
