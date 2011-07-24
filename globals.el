eab4527c2a8aa06b950fc65328b93de9814bf5c0
Vedat Hallac <vedathallac@gmail.com>
2011-07-25 02:15:41 +0300

    Small tidy up
    - Deleted left-over debug messages
    - Re-indented slick-copy advice

--- globals.el
+++ globals.el
@@ -3,9 +3,7 @@
 
 (add-hook 'after-make-frame-functions
           (lambda (frame)
-            (message "Setting it up")
              (when (window-system frame)
-               (message "Should get rid of scroll bar here")
                (scroll-bar-mode -1)
                (set-cursor-color "light green"))
              (blink-cursor-mode -1)
@@ -26,11 +24,12 @@
 ;; Interactively do things
 (ido-mode t)
 
-(defadvice kill-ring-save (before slick-copy activate compile) "When called
-  interactively with no active region, copy a single line instead."
-  (interactive (if mark-active (list (region-beginning) (region-end)) (message
-  "Copied line") (list (line-beginning-position) (line-beginning-position
-  2)))))
+(defadvice kill-ring-save (before slick-copy activate compile)
+  "When called interactively with no active region, copy a single line instead."
+  (interactive
+   (if mark-active (list (region-beginning) (region-end))
+     (message "Copied line")
+     (list (line-beginning-position) (line-beginning-position 2)))))
 
 (defadvice kill-region (before slick-cut activate compile)
   "When called interactively with no active region, kill a single line instead."
