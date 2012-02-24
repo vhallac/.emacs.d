fa62f050e90172bb3f17ce224bcd54b5df63a2d7
Vedat Hallac <vedathallac@gmail.com>
2012-02-24 10:07:11 +0200

    Changed font to 'Envy Code R'

--- platform/gnu/linux/ui.el
+++ platform/gnu/linux/ui.el
@@ -1,19 +1,24 @@
 (defun setup-initial-frame (&optional frame)
   (when (window-system frame)
     (message "Adjusting!!!")
+    (setup-ui-face frame)
     (if (>= (x-display-pixel-height) 800)
         (setq initial-frame-alist '((height . 55))))))
 
-(defun setup-frame-height (&optional frame)
+(defun setup-ui-frame (&optional frame)
   (when (window-system frame)
     ;; The calculation below is based on rough trial/error.
     (set-frame-height frame (/ (x-display-pixel-height) 19))
+    (setup-ui-face frame)))
+
+(defun setup-ui-face (&optional frame)
     ;; Emacs discards my font configuration from ~/.emacs.d/customizations.el
     ;; Adding a replica here to ensure it gets set properly.
-    (set-face-attribute 'default nil :height 98 :foundry "outline" :family "Envy Code R")))
+    (set-face-font 'default "Envy Code R")
+    (set-face-attribute 'default nil :height 98))
 
 ;; If not in daemon mode, just  setup te initial frame
 ;; Otherwise, add the hook to set the frame up
 (if (not (daemonp))
     (setup-initial-frame)
-  (add-hook 'after-make-frame-functions 'setup-frame-height))
+  (add-hook 'after-make-frame-functions 'setup-ui-frame))
