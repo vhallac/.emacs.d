a45891ac7f22c75743c583a3df5844d83ee06e53
Vedat Hallac <vedathallac@gmail.com>
2012-02-25 17:16:14 +0200

    Start/stop eclimd from within emacs

--- config/customize-eclim.el
+++ config/customize-eclim.el
@@ -4,7 +4,8 @@
 (setq eclim-auto-save t 		; Need to save before analyzing class
       help-at-pt-display-when-idle t
       eclim-executable (concat eclipse-dir "eclim")
-)
+      eclimd-executable (concat eclipse-dir "eclimd")
+      eclimd-workspace "~/work")
 
 (help-at-pt-set-timer)
 (add-hook 'eclim-mode-hook
@@ -13,3 +14,23 @@
 	    (ac-config-default)
 	    (add-to-list 'ac-sources 'ac-source-eclim))) ; ac-source-emacs-eclim is also available
 
+(defconst eclimd-process-buffer-name "eclimd")
+
+(defun start-eclimd (workspace-dir)
+  (interactive "D")
+  (make-comint eclimd-process-buffer-name
+	       eclimd-executable
+	       nil
+	       (concat "-Dosgi.instance.area.default="
+		       (replace-regexp-in-string "~" "@user.home" workspace-dir))))
+
+(defun stop-eclimd ()
+  (interactive)
+  (let* ((eclimd-buffer (get-buffer (concat "*" eclimd-process-buffer-name "*")))
+	 (eclimd-proc (get-buffer-process eclimd-buffer)))
+    (when eclimd-proc
+      (delete-process eclimd-proc))
+    (when eclimd-buffer
+      (kill-buffer eclimd-buffer))))
+
+(start-eclimd eclimd-default-workspace)
