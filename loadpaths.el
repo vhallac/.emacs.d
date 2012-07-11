2e04b2d04bec5ad33cbd947a8216777ab1bd2aec
Vedat Hallac <vedat@daredevil.home.lan>
2012-07-11 13:01:39 +0300

    Use backport-package.el to get a better package.el version.
    - Added tiny infrastructure to be able to add some backport packages
    - Moved output of backport-package.el to emacs23 compatibility directory

--- loadpaths.el
+++ loadpaths.el
@@ -12,8 +12,23 @@
         (setq toplevel (cdr toplevel))))
     (setq dirs (append dirs (list path)))))
 
+(defun get-compat-dirs ()
+  (let* ((compat-dir (expand-file-name "emacs-compat"
+                                       (or user-emacs-directory
+                                           "~/.emacs.d/")))
+         (major-compat-dir (expand-file-name
+                            (concat "emacs-"
+                                    (number-to-string emacs-major-version))
+                            compat-dir))
+         (default-compat-dir (expand-file-name "default"
+                                               compat-dir)))
+    (delq nil
+          (list (and (file-directory-p major-compat-dir) major-compat-dir)
+                (when (file-directory-p default-compat-dir) default-compat-dir)))))
+
 ;; My packages have load priority
 (setq load-path (append
+                 (get-compat-dirs)
                  (recursive-directory-list "~/.emacs.d/lisp")
                  (recursive-directory-list "~/.emacs.d/vhallac-lisp")
                  load-path))
