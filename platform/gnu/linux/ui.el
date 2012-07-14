cc9324cae6ec39169af25eae7dda95a115428b22
Vedat Hallac <vedat@daredevil.home.lan>
2012-07-14 12:49:02 +0300

    Bugfix for daemon mode and X
    Looks like I need to select the new frame manually when emacs is in daemon mode,
    and I am creating a new UI frame. Otherwise the fonts cannot be loaded.

--- platform/gnu/linux/ui.el
+++ platform/gnu/linux/ui.el
@@ -14,8 +14,9 @@
 (defun setup-ui-face (&optional frame)
     ;; Emacs discards my font configuration from ~/.emacs.d/customizations.el
     ;; Adding a replica here to ensure it gets set properly.
-    (set-face-font 'default "Envy Code R")
-    (set-face-attribute 'default nil :height 98))
+  (when frame (select-frame frame))
+  (set-face-font 'default "Envy Code R")
+  (set-face-attribute 'default nil :height 98))
 
 ;; If not in daemon mode, just  setup te initial frame
 ;; Otherwise, add the hook to set the frame up
