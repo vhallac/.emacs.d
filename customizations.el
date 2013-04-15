a93f48be924f7f08b619555095a3507b6dfcbee2
Vedat Hallac <vedat@sleek15-1.(none)>
2013-04-15 15:08:12 +0300

    SystemWindowText is not defined in debian/ubuntu. Just say "black"

--- customizations.el
+++ customizations.el
@@ -12,4 +12,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "wheat" :foreground "SystemWindowText" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "outline" :family "Courier New")))))
+ '(default ((t (:inherit nil :stipple nil :background "wheat" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "outline" :family "Courier New")))))
