ec04838827db45467225fc32bc0179f7c52a0dfa
Vedat Hallac <vedat.hallac@pia-team.com>
2013-09-19 11:45:27 +0300

    Use the to/cc/... fields to determine my e-mail address to use when replying.

---
+++ config/customize-message.el
@@ -0,0 +1,7 @@
+(setq message-alternative-emails (regexp-opt '("vedathallac@gmail.com"
+                                               "vedat.hallac@gmail.com"
+                                               "dys.wowace@gmail.com"
+                                               "vedat@android.ciyiz.biz"
+                                               "vedat@oyun.cuyuz.biz"
+                                               "vedathallac@yandex.com"
+                                               "vedat.hallac@pia-team.com")))
