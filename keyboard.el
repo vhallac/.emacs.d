270cca128688ab71707ffe296082675f36418755
Vedat Hallac <vedat.hallac@pia-team.com>
2013-09-19 11:49:07 +0300

    Go with more standard key bindings

--- keyboard.el
+++ keyboard.el
@@ -7,7 +7,6 @@
 (define-key global-map (kbd "C-<home>") 'beginning-of-buffer)
 (define-key global-map (kbd "C-j") 'delete-indentation)
 (define-key global-map (kbd "C-S-w") 'delete-region)
-(define-key global-map (kbd "M-g") 'goto-line)
 (define-key global-map (kbd "<f9>") 'electric-buffer-list)
 (define-key global-map (kbd "M-]") 'match-parenthesis)
 (define-key global-map (kbd "M-s M-s") 'other-window)
@@ -17,7 +16,7 @@
 
 (define-key global-map (kbd "C-x C-j C-c") 'jabber-connect-all)
 
-(define-key global-map (kbd "C-M-r") 'org-capture)
+(define-key global-map (kbd "C-c c") 'org-capture)
 (define-key global-map (kbd "C-c l") 'org-store-link)
 (define-key global-map (kbd "C-c a") 'org-agenda)
 (define-key global-map (kbd "<f12>") 'org-agenda)
