2d4d4d5f042964763152856e716aeb1c40aee5f6
Vedat Hallac <vedat.hallac@pia-team.com>
2013-10-06 17:45:42 +0300

    Add HTML mail signature for PiA

--- config/customize-message.el
+++ config/customize-message.el
@@ -13,6 +13,24 @@
                                                "vedat@oyun.cuyuz.biz"
                                                "vedathallac@yandex.com"
                                                "vedat.hallac@pia-team.com")))
+(defun pia-insert-html-sig ()
+  (interactive)
+  (insert-string
+   (base64-decode-string
+    ;; Abusing base64 to avoid escaping the quotes. :)
+    (concat "PGRpdiBzdHlsZT0icGFkZGluZy10b3A6N3B4O2ZvbnQtZmFtaWx5OidWZXJkYW5hJywnc2Fucy1z"
+            "ZXJpZic7Zm9udC1zaXplOjhwdCI+PGEgaHJlZj0iaHR0cDovL3d3dy5waWEtdGVhbS5jb20vIiB0"
+            "YXJnZXQ9Il9ibGFuayI+PGltZyBzcmM9Imh0dHA6Ly93d3cucGlhLXRlYW0uY29tL3dwLWNvbnRl"
+            "bnQvdXBsb2Fkcy8yMDEyLzA5L2xvZ29fZmluYWwucG5nIiBoZWlnaHQ9IjcyIiB3aWR0aD0iMTYw"
+            "Ii8+PC9hPjxociB3aWR0aD0iMTYwIiBhbGlnbj0ibGVmdCIvPjxwIGFsaWduPSJsZWZ0Ij48c3Bh"
+            "biBzdHlsZT0iZm9udC1zaXplOjlwdCIgbGFuZz0iRU4tVVMiPjxiPkFobWV0IFZlZGF0IEhhbGxh"
+            "JiMyMzE7PC9iPjwvc3Bhbj48YnIvPlNlbmlvciBTb2Z0d2FyZSBFbmdpbmVlcjxici8+PGJyLz5N"
+            "OiArOTAgNTQxIDgzMyAyOCA4Mjxici8+VDogKzkwIDIxNiA2ODggNjkgNDE8YnIvPkJ1eWFrYSBB"
+            "Vk0gLCBCIGJsb2sgS2F0IDEyIE5vOjcyPGJyLz4zNDc3MSBUZXBlJiMyNTI7c3QmIzI1MjsgLSAm"
+            "IzIyMDttcmFuaXllIC0gJiMzMDQ7c3RhbmJ1bDxici8+PHNwYW4gc3R5bGU9ImNvbG9yOiMwMDY4"
+            "Y2Y7Ij48YSBocmVmPSJtYWlsdG86dmVkYXQuaGFsbGFjQHBpYS10ZWFtLmNvbSI+PHU+dmVkYXQu"
+            "aGFsbGFjQHBpYS10ZWFtLmNvbTwvdT48L2E+PC9zcGFuPjwvcD48L2Rpdj4="))))
+
 (defun vh-message-edit-body-as-org ()
   "Edit the body of the message in org-mode.
 
