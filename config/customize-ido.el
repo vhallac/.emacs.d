cec437d36d554b33f302738a32266752b6f7296d
Vedat Hallac <vedat.hallac@pia-team.com>
2014-01-03 14:02:55 +0200

    Added ido vertical and flx mods

--- config/customize-ido.el
+++ config/customize-ido.el
@@ -2,4 +2,9 @@
   (require 'ido))
 
 ;; Nice vertical display. Default setup gives me claustrophobia.
-(ido-vertical-mode t)
+(when (boundp ido-vertical-mode)
+  (ido-vertical-mode))
+
+;; Allow flexible matching in ido
+(when (boundp flx-ido-mode)
+  (flx-ido-mode))
