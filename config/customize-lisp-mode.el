7b11c59b1fdfe9964b98152d458c151f5e53ea7b
Vedat Hallac <vedat.hallac@pia-team.com>
2014-07-03 15:35:32 +0300

    Enable paredit mode in lisp by default

---
+++ config/customize-lisp-mode.el
@@ -0,0 +1,6 @@
+(eval-when-compile
+  (require 'lisp-mode))
+
+(add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
