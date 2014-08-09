2538fef46064d3d24183338606c16dff3ee161a8
Vedat Hallac <vedat.hallac@pia-team.com>
2014-08-09 08:57:18 +0300

    Advice the ledger-add-transaction for indent fix
    When adding a transaction with transaction copy, ledger left 4 spaces
    for indent instead of my configured 2. Added an advice to indent the new
    transaction after it is added.

--- config/customize-ledger-mode.el
+++ config/customize-ledger-mode.el
@@ -4,3 +4,9 @@
 (setq ledger-post-account-alignment-column 2
       ledger-clear-whole-transactions t
       ledger-complete-ignore-case t)
+
+(defadvice ledger-add-transaction (after ledger-add-transaction-indent activate compile)
+  "Indent the newly added transaction to ensure indent amount is correct."
+  (let ((extens (ledger-find-xact-extents (point))))
+    (when extens
+      (apply #'indent-region extens))))
