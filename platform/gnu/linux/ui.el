35ccee6b95f5fe8cc5142a389f5766c6a888a1f3
Vedat Hallac <vedat.hallac@pia-team.com>
2014-08-18 09:19:13 +0300

    Get gpg-agent to work when emacs starts as daemon after reboot

--- platform/gnu/linux/ui.el
+++ platform/gnu/linux/ui.el
@@ -22,3 +22,19 @@
 (if (not (daemonp))
     (setup-initial-frame)
   (add-hook 'after-make-frame-functions 'setup-ui-frame))
+
+(defun wg/kludge-gpg-agent ()
+  ;; For this to work, after you start gpg-agent, dump the contents of file
+  ;; to /tmp/vedat.gpgagent.info file
+  (with-temp-buffer
+    (insert-file-contents "/tmp/vedat.gpgagent.info")
+    (when (/= (point-min) (point-max))
+      (setenv "GPG_AGENT_INFO" (buffer-substring (point-min) (point-at-eol)))))
+  (if (display-graphic-p)
+      (setenv "DISPLAY"
+              (terminal-name))
+    (setenv "GPG_TTY"
+            (terminal-name))
+    (setenv "DISPLAY")))
+
+(add-hook 'window-configuration-change-hook 'wg/kludge-gpg-agent)
