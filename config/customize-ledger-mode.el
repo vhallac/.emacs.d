7d203470debd5667c4dc5678939bdc35c7c3b4d9
Vedat Hallac <vedat.hallac@pia-team.com>
2015-10-27 09:14:10 +0300

    Update ledger mode
    The mode glitch for the transaction indentation is fixed now. Removing
    advice definition.
    Removing transaction highlighting. It is distracting.

--- config/customize-ledger-mode.el
+++ config/customize-ledger-mode.el
@@ -3,10 +3,5 @@
 
 (setq ledger-post-account-alignment-column 2
       ledger-clear-whole-transactions t
-      ledger-complete-ignore-case t)
-
-(defadvice ledger-add-transaction (after ledger-add-transaction-indent activate compile)
-  "Indent the newly added transaction to ensure indent amount is correct."
-  (let ((extens (ledger-find-xact-extents (point))))
-    (when extens
-      (apply #'indent-region extens))))
+      ledger-complete-ignore-case t
+      ledger-highlight-xact-under-point nil)
