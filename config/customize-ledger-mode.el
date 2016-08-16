f77fe1d4d103843da63ee0d294030c6e473ce25c
Vedat Hallac <vedat.hallac@pia-team.com>
2016-08-16 10:52:38 +0300

    Prevent ledger increasing newlines at EOF

--- config/customize-ledger-mode.el
+++ config/customize-ledger-mode.el
@@ -5,3 +5,7 @@
       ledger-clear-whole-transactions t
       ledger-complete-ignore-case t
       ledger-highlight-xact-under-point nil)
+
+(defadvice ledger-add-transaction (after remove-extra-newlines activate)
+  (when (looking-at "\n\n\n")
+    (delete-char 2)))
